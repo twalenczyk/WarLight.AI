69bcfaea38cf49ca3bb60ae4e85d569d90c5f1eb
Taylor Walenczyk <taylor.walenczyk@garmin.com>
2020-05-05 10:36:32 -0500

    Added support for comprehensive data storage and parsing for deployments

--- Snowbird/Parser.cs
+++ Snowbird/Parser.cs
@@ -12,7 +12,6 @@ namespace WarLight.Shared.AI.Snowbird
 {
     public static class Parser
     {
-
         /*
          * *************************
          * Attack Deployment Methods
@@ -57,35 +56,7 @@ namespace WarLight.Shared.AI.Snowbird
             else
             {
                 // consolidate the data from the set of current games on file
-                var rawData = new List<Dictionary<TerritoryIDType, List<double>>>();
-                dir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "DataCollection//Raw//Maps//" + mapID.ToString() + "//AttackDeployments");
-
-                foreach (var gameFile in Directory.GetFiles(dir))
-                {
-                    var deploymentDataPerTurn = ParseDeploymentGame(gameFile);
-
-                    for (var i = 0; i < deploymentDataPerTurn.Count; i++)
-                    {
-                        if (rawData.Count <= i)
-                        {
-                            rawData.Add(deploymentDataPerTurn[i]);
-                        }
-                        else
-                        {
-                            deploymentDataPerTurn[i].ForEach(kvp =>
-                            {
-                                if (rawData[i].ContainsKey(kvp.Key))
-                                {
-                                    rawData[i][kvp.Key].AddRange(kvp.Value);
-                                }
-                                else
-                                {
-                                    rawData[i][kvp.Key] = kvp.Value;
-                                }
-                            });
-                        }
-                    }
-                }
+                var rawData = GetDeploymentComprehensiveData(mapID, true);
 
                 // consider storing the rawData collection somewhere
                 ret = rawData.Select(
@@ -96,8 +67,49 @@ namespace WarLight.Shared.AI.Snowbird
                         .ToList();
 
                 // write the data for future use
-                DataCollector.WriteMapAttackDeploymentMeansComprehensiveData(rawData, mapID);
                 DataCollector.WriteMapAttackDeploymentMeans(ret, mapID);
+            }
+
+            return ret;
+        }
+
+        public static List<Dictionary<TerritoryIDType, List<double>>> GetAttackDeploymentsMeansComprehensiveData(MapIDType mapID)
+        {
+            var ret = new List<Dictionary<TerritoryIDType, List<double>>>();
+
+            var dir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "DataCollection//Consolidated//Maps//" + mapID.ToString() + "//AttackDeployments");
+            var fp = Path.Combine("means_comp.txt");
+
+            if (File.Exists(fp))
+            {
+                // Simply parse the relevant file
+                var text = File.ReadAllText(Path.Combine(fp));
+                text = text.Replace("\n", string.Empty).Replace("\r", string.Empty);
+                var check = text.Split('!');
+                foreach (var chunk in text.Split('!').Where(s => s != string.Empty))
+                {
+                    var data = JObject.Parse(chunk);
+                    var turnNumber = data["turnNumber"].Value<int>();
+                    var borderArmies = data["deployments"];
+
+                    // in case turn numbers are unordered/missing
+                    for (var i = ret.Count; i <= turnNumber; i++)
+                    {
+                        ret.Add(new Dictionary<TerritoryIDType, List<double>>());
+                    }
+
+                    foreach (var border in borderArmies)
+                    {
+                        var id = (TerritoryIDType)border["territoryID"].Value<int>();
+                        var armies = border["armies"].Select(token => token.Value<double>()).ToList();
+                        ret[turnNumber][id] = armies;
+                    }
+                }
+            }
+            else
+            {
+                // create the file from relevant game files on the map
+                ret = GetDeploymentComprehensiveData(mapID, true);
             }
 
             return ret;
@@ -155,35 +167,7 @@ namespace WarLight.Shared.AI.Snowbird
             else
             {
                 // consolidate the data from the set of current games on file
-                var rawData = new List<Dictionary<TerritoryIDType, List<double>>>();
-                dir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "DataCollection//Raw//Maps//" + mapID.ToString() + "//DefenseDeployments");
-                
-                foreach (var gameFile in Directory.GetFiles(dir))
-                {
-                    var deploymentDataPerTurn = ParseDeploymentGame(gameFile);
-
-                    for (var i = 0; i < deploymentDataPerTurn.Count; i++)
-                    {
-                        if (rawData.Count <= i)
-                        {
-                            rawData.Add(deploymentDataPerTurn[i]);
-                        }
-                        else
-                        {
-                            deploymentDataPerTurn[i].ForEach(kvp =>
-                            {
-                                if (rawData[i].ContainsKey(kvp.Key))
-                                {
-                                    rawData[i][kvp.Key].AddRange(kvp.Value);
-                                }
-                                else
-                                {
-                                    rawData[i][kvp.Key] = kvp.Value;
-                                }
-                            });
-                        }
-                    }
-                }
+                var rawData = GetDeploymentComprehensiveData(mapID, false);
 
                 // consider storing the rawData collection somewhere
                 ret = rawData.Select(
@@ -194,13 +178,53 @@ namespace WarLight.Shared.AI.Snowbird
                         .ToList();
 
                 // write the data for future use
-                DataCollector.WriteMapDefenseDeploymentMeansComprehensiveData(rawData, mapID);
                 DataCollector.WriteMapDefenseDeploymentMeans(ret, mapID);
             }
 
             return ret;
         }
 
+        public static List<Dictionary<TerritoryIDType, List<double>>> GetDefenseDeploymentsMeansComprehensiveData(MapIDType mapID)
+        {
+            var ret = new List<Dictionary<TerritoryIDType, List<double>>>();
+
+            var dir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "DataCollection//Consolidated//Maps//" + mapID.ToString() + "//DefenseDeployments");
+            var fp = Path.Combine("means_comp.txt");
+
+            if (File.Exists(fp))
+            {
+                // Simply parse the relevant file
+                var text = File.ReadAllText(Path.Combine(fp));
+                text = text.Replace("\n", string.Empty).Replace("\r", string.Empty);
+                var check = text.Split('!');
+                foreach (var chunk in text.Split('!').Where(s => s != string.Empty))
+                {
+                    var data = JObject.Parse(chunk);
+                    var turnNumber = data["turnNumber"].Value<int>();
+                    var borderArmies = data["deployments"];
+
+                    // in case turn numbers are unordered/missing
+                    for (var i = ret.Count; i <= turnNumber; i++)
+                    {
+                        ret.Add(new Dictionary<TerritoryIDType, List<double>>());
+                    }
+
+                    foreach (var border in borderArmies)
+                    {
+                        var id = (TerritoryIDType)border["territoryID"].Value<int>();
+                        var armies = border["armies"].Select(token => token.Value<double>()).ToList();
+                        ret[turnNumber][id] = armies;
+                    }
+                }
+            }
+            else
+            {
+                // create the file from relevant game files on the map
+                ret = GetDeploymentComprehensiveData(mapID, false);
+            }
+
+            return ret;
+        }
 
         /*
          * *************************
@@ -374,6 +398,43 @@ namespace WarLight.Shared.AI.Snowbird
             }
 
             return ret;
+        }
+
+        private static List<Dictionary<TerritoryIDType, List<double>>> GetDeploymentComprehensiveData(MapIDType mapID, bool isAttackQuery)
+        {
+            var rawData = new List<Dictionary<TerritoryIDType, List<double>>>();
+            var dirEnding = isAttackQuery ? "//AttackDeployments" : "//DefenseDeployments";
+            var dir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "DataCollection//Raw//Maps//" + mapID.ToString() + dirEnding);
+
+            foreach (var gameFile in Directory.GetFiles(dir))
+            {
+                var deploymentDataPerTurn = ParseDeploymentGame(gameFile);
+
+                for (var i = 0; i < deploymentDataPerTurn.Count; i++)
+                {
+                    if (rawData.Count <= i)
+                    {
+                        rawData.Add(deploymentDataPerTurn[i]);
+                    }
+                    else
+                    {
+                        deploymentDataPerTurn[i].ForEach(kvp =>
+                        {
+                            if (rawData[i].ContainsKey(kvp.Key))
+                            {
+                                rawData[i][kvp.Key].AddRange(kvp.Value);
+                            }
+                            else
+                            {
+                                rawData[i][kvp.Key] = kvp.Value;
+                            }
+                        });
+                    }
+                }
+            }
+
+            DataCollector.WriteMapDefenseDeploymentMeansComprehensiveData(rawData, mapID);
+            return rawData;
         }
     }
 }
